eac8319d989e1a742fe193d7be352348747ce046
Alejandro Depetris <aledepetris@outlook.com>
2023-04-05 13:39:59 -0300

    Se incorporó la funcionalidad de agregar y editar y se añadieron las pantallas y logica pertinentes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-agregar',
@@ -29,9 +32,39 @@ export class AgregarComponent implements OnInit {
     alt_img: ''
   }
 
-  constructor() { }
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router ) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroesPorId( id ))
+      )
+      .subscribe( heroe => this.heroe = heroe )
   }
 
+  guardar() {
+    if ( this.heroe.superhero.trim().length == 0) {
+      return;
+    }
+
+    if ( this.heroe.id ) {
+      // Editar
+      this.heroesService.actualizarHeroe( this.heroe )
+        .subscribe( heroe => {
+          console.log('Actualizado:', heroe)
+        })
+
+    } else {
+      // Crear
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe( heroe => {
+        this.router.navigate(['/heroes/editar', heroe.id]);
+      })
+
+    }
+
+  }
 }
